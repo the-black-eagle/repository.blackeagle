6ff9dac521ad03d5e48bad86507d00d8f7c3db59
the-black-eagle <g.moore@gmx.co.uk>
2018-08-28 15:13:52 +0100

    Updated

--- script.database.cleaner/default.py
+++ script.database.cleaner/default.py
@@ -17,17 +17,17 @@
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
 #  (at your option) any later version.
-#  
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
-#  
+#
 
 
 import datetime
@@ -58,137 +58,91 @@ WINDOW = xbmcgui.Window(10000)
 
 class MyClass(xbmcgui.WindowXMLDialog):
     def __init__( self, *args, **kwargs  ): pass
-    
+
     def onInit( self ):
-        self.setCoordinateResolution(0)
         self.container = self.getControl(6)
         self.container2 = self.getControl(8)
         self.container3 = self.getControl(10)
-        self.container4 = self.getControl(12)
         self.listitems = []
         self.excludesitems = []
         self.addonsettings = []
-        self.addonsettings_on_off = []
-        
-    #       List paths from sources.xml 
+
+    #       List paths from sources.xml
         if not specificpath and not replacepath:
             self.display_list = display_list
             for i in range(len(self.display_list)):
-                self.listitems.append(self.display_list[i])
+                self.listitems.append('[COLOR yellow]'+ self.display_list[i] + '[/COLOR]')
             if no_sources:
-                self.listitems.append('No sources are in use')
-                self.listitems.append('All streaming paths will be removed')
+                self.listitems.append('[COLOR red][B]No sources are in use[/B][/COLOR]')
+                self.listitems.append('[COLOR red][B]All streaming paths will be removed[/B][/COLOR]')
                 if excluding:
-                    self.listitems.append('')
-                    self.listitems.append('Paths from excludes.xml will be kept')
+					self.listitems.append('')
+					self.listitems.append('[COLOR red][B]Paths from excludes.xml will be kept[/B][/COLOR]')
         if replacepath:
-            self.listitems.append('Replacing a path')
-            self.listitems.append('in your database')
-            self.listitems.append('Confirm details below')
+            self.listitems.append('[COLOR yellow]Replacing a path[/COLOR]')
+            self.listitems.append('[COLOR yellow]in your database[/COLOR]')
+            self.listitems.append('[COLOR yellow]Confirm details below[/COLOR]')
         if specificpath:
-            self.listitems.append('Removing a path')
-            self.listitems.append('in your database')
-            self.listitems.append('Confirm details below')
+            self.listitems.append('[COLOR yellow]Removing a path[/COLOR]')
+            self.listitems.append('[COLOR yellow]in your database[/COLOR]')
+            self.listitems.append('[COLOR yellow]Confirm details below[/COLOR]')
         self.container.addItems(self.listitems)
     #       List paths in excludes.xml (if it exists)
-        
         self.excludes_list = excludes_list
 
         if excluding:
             for i in range(len(self.excludes_list)):
-                self.excludesitems.append(self.excludes_list[i])
+                self.excludesitems.append('[COLOR yellow]' + self.excludes_list[i]+ '[/COLOR]')
         else:
             self.excludesitems.append("Not Present")
         self.container2.addItems(self.excludesitems)
-            
+
     #       List the relevant addon settings
- 
+
         if is_pvr and (not specificpath and not replacepath):
             self.addonsettings.append('Keep PVR information')
-            self.addonsettings_on_off.append('ON')
-        else:
-            self.addonsettings.append('Keep PVR information')
-            self.addonsettings_on_off.append('OFF')
-
         if bookmarks and (not specificpath and not replacepath):
             self.addonsettings.append('Keep bookmark information')
-            self.addonsettings_on_off.append('ON')
-        else:
-            self.addonsettings.append('Keep bookmark information')
-            self.addonsettings_on_off.append('OFF')
-
         if autoclean:
-            self.addonsettings.append('Automatically trigger Clean Library')
-            self.addonsettings_on_off.append('ON')
-        else:
-            self.addonsettings.append('Automatically trigger Clean Library')
-            self.addonsettings_on_off.append('OFF')
+            self.addonsettings.append('Auto call Clean Library')
         if promptdelete:
-            self.addonsettings.append('Show summary window')
-            self.addonsettings_on_off.append('ON')
-        else:
-            self.addonsettings.append('Show summary window')
-            self.addonsettings_on_off.append('OFF')
+            self.addonsettings.append('Show summary window (This window !!)')
         if autobackup == 'true' and not is_mysql:
-            self.addonsettings.append('Back up local database before cleaning')
-            self.addonsettings_on_off.append('ON')
-        else:
-            self.addonsettings.append('Back up local database before cleaning')
-            self.addonsettings_on_off.append('OFF')
+            self.addonsettings.append('Auto backing up local database')
         if no_sources or specificpath or replacepath:
-            self.addonsettings.append('Use sources.xml')
-            self.addonsettings_on_off.append('OFF')
-        else:
-            self.addonsettings.append('Use sources.xml')
-            self.addonsettings_on_off.append('ON')
+            self.addonsettings.append('[COLOR red]Not[/COLOR] using info from sources.xml')
         if specificpath:
-            self.addonsettings.append('Cleaning a specific path')
-            self.addonsettings_on_off.append('ON')
-        else:
-            self.addonsettings.append('Cleaning a specific path')
-            self.addonsettings_on_off.append('OFF')
+            self.addonsettings.append('[COLOR red]Cleaning a specific path[/COLOR]')
         if replacepath:
-            self.addonsettings.append('Replacing a path')
-            self.addonsettings_on_off.append('ON')
-        else:
-            self.addonsettings.append('Replacing a path')
-            self.addonsettings_on_off.append('OFF')
+            self.addonsettings.append('[COLOR red]Replacing a path[/COLOR]')
         if enable_logging:
-            self.addonsettings.append('Log paths to database-cleaner.log')
-            self.addonsettings_on_off.append('ON')
-        else:
-            self.addonsettings.append('Log paths to database-cleaner.log')
-            self.addonsettings_on_off.append('OFF')
+            self.addonsettings.append('Writing a logfile to Kodi TEMP directory')
         if debugging:
-            self.addonsettings.append('Enable debugging to Kodi debug log')
-            self.addonsettings_on_off.append('ON')
+            debug_string = 'Debugging [COLOR red]enabled[/COLOR]'
         else:
-            self.addonsettings.append('Enable debugging Kodi debug log')
-            self.addonsettings_on_off.append('OFF')
-         #   Display the name of the database we are connected to
-        self.addonsettings.append('Database')
-        self.addonsettings_on_off.append(our_dbname)
-
+            debug_string = 'Debugging [COLOR green]disabled[/COLOR]'
+        self.addonsettings.append(debug_string)
+        self.addonsettings.append('') #blank line
+        #   Display the name of the database we are connected to
+        self.addonsettings.append('Database is - [COLOR green][B]%s[/B][/COLOR]' % our_dbname)
         cursor.execute(our_select)
         data_list = cursor.fetchall()
         data_list_size = len(data_list)
         if replacepath:
-            self.getControl(22).setLabel('There are %d paths to be changed' % data_list_size)
+            self.addonsettings.append('[COLOR red][B]There are %d paths to be changed[/B][/COLOR]' % data_list_size)
         else:
-            self.getControl(22).setLabel('There are %d entries to be removed' % data_list_size)
+            self.addonsettings.append('[COLOR red][B]There are %d entries to be removed[/B][/COLOR]' % data_list_size)
 
         self.container3.addItems(self.addonsettings)
-        self.container4.addItems(self.addonsettings_on_off)
-        #   Show warning about backup if using MySQL    
+        #   Show warning about backup if using MySQL
         if is_mysql:
-            self.getControl(20).setLabel('WARNING - MySQL database not backed up automatically, please do this manually')
+            self.getControl(20).setLabel('WARNING - MySQL database [COLOR red][B]not[/B][/COLOR] backed up automatically, please do this [B]manually[/B]')
         if specificpath:
-            self.getControl(20).setLabel("WARNING - Removing specific path '%s' " % specific_path_to_remove)
+            self.getControl(20).setLabel('WARNING - Removing specific path [COLOR yellow]%s[/COLOR] ' % specific_path_to_remove)
         if replacepath:
-            self.getControl(20).setLabel("WARNING - Renaming specific path from '%s' " % old_path)
-            self.getControl(21).setLabel("TO '%s' " % new_path)
+            self.getControl(20).setLabel('WARNING - Renaming specific path from [COLOR yellow]%s[/COLOR] ' % old_path)
+            self.getControl(21).setLabel('TO  [COLOR yellow]%s[/COLOR] ' % new_path)
 
- 
     def onAction(self, action):
         global flag
         dbglog('Got an action %s' % action.getId())
@@ -240,7 +194,6 @@ enable_logging = addon.getSetting('logtolog')
 if enable_logging == 'true':
     enable_logging = True
     type_of_log = addon.getSetting('typeoflog')
-    
 else:
     enable_logging = False
 if replacepath == 'true':
@@ -278,22 +231,20 @@ else:
 
 
 def log(txt):
-    
+
     if isinstance(txt, str):
         txt = txt.decode('utf-8')
         message = u'%s: %s' % (addonname, txt)
-        xbmc.log(msg=message.encode('utf-8'), level=xbmc.LOGDEBUG)  
-    
+        xbmc.log(msg=message.encode('utf-8'), level=xbmc.LOGDEBUG)
+
 def dbglog(txt):
     if debugging:
         log(txt)
-        
+
 def exit_on_error():
     WINDOW.setProperty('database-cleaner-running', 'false')
     exit(1)
 
-        
-        
 def cleaner_log_file(our_select, cleaning):
     cleaner_log = xbmc.translatePath('special://temp/database-cleaner.log').decode('utf-8')
     old_cleaner_log = xbmc.translatePath('special://temp/database-cleaner.old.log').decode('utf-8')
@@ -321,7 +272,7 @@ def cleaner_log_file(our_select, cleaning):
         old_log= xbmcvfs.File(cleaner_log)
         old_log_contents=old_log.read()
         old_log.close()
-    
+
     now = datetime.datetime.now()
     logfile=xbmcvfs.File(cleaner_log, 'w')
     if old_log_contents:
@@ -343,7 +294,7 @@ def cleaner_log_file(our_select, cleaning):
         dbglog('Creating progress dialog for logfile')
         dialog.create('Getting required data.  Please wait')
         dialog.update(1)
-    
+
     if not cleaning and not replacepath:
         logfile.write('The following file paths would be removed from your database')
         logfile.write('\n\n')
@@ -391,19 +342,19 @@ def cleaner_log_file(our_select, cleaning):
     if counting == 0:  # nothing to remove
         logfile.write('No paths have been found to remove\n')
     if do_progress:
-        dialog.close()  
+        dialog.close()
     logfile.write('\n\n')
     logfile.close()
-    
+
 ####    Start Here !!   ####
 
 dbglog('script version %s started' % addonversion)
-if WINDOW.getProperty('database-cleaner-running') == 'true': 
-    log('Video Database Cleaner already running')
-    exit(0)
-else:
-    WINDOW.setProperty('database-cleaner-running', 'true')
-    
+#if WINDOW.getProperty('database-cleaner-running') == 'true':
+    #log('Video Database Cleaner already running')
+    #exit(0)
+#else:
+    #WINDOW.setProperty('database-cleaner-running', 'true')
+
 xbmcgui.Dialog().notification(addonname, 'Starting Up', xbmcgui.NOTIFICATION_INFO, 2000)
 xbmc.sleep(2000)
 
@@ -431,10 +382,14 @@ if found:
             except:
                 log('Unable to determine MySQL password')
             try:
+                our_port = videodb.find('port').text
+            except:
+                log('Unable to find MySQL port')
+            try:
                 our_dbname = videodb.find('name').text
             except:
                 our_dbname = 'MyVideos'
-            dbglog('MySQL details - %s, %s, %s' % (our_host, our_username, our_dbname))
+            dbglog('MySQL details - %s, %s, %s, %s' % (our_host, our_port, our_username, our_dbname))
             is_mysql = True
     except Exception as e:
         e =str(e)
@@ -444,7 +399,7 @@ if found:
 if not is_mysql:
     our_dbname = 'MyVideos'
 
-    for num in range(120, 35, -1):
+    for num in range(114, 35, -1):
         testname = our_dbname + str(num)
         our_test = db_path + testname + '.db'
 
@@ -492,8 +447,6 @@ if promptdelete:
 if no_sources:
     dbglog('Not using sources.xml')
 
-
-    
 if source_file_path != '':
     sources_file = source_file_path
     remote_file = True
@@ -535,7 +488,7 @@ if forcedbname:
 if is_mysql and not forcedbname:
     if our_dbname == '': # no db name in advancedsettings
         our_dbname = 'MyVideos'
-        for num in range(120, 35, -1):
+        for num in range(114, 35, -1):
             testname = our_dbname + str(num)
             try:
                 dbglog('Attempting MySQL connection to %s' % testname)
@@ -549,11 +502,11 @@ if is_mysql and not forcedbname:
             except:
                 pass
     else:       # already got db name from ad settings
-        for num in range(120, 35, -1):
+        for num in range(114, 35, -1):
             testname = our_dbname + str(num)
             try:
                 dbglog('Attempting MySQL connection to %s' % testname)
-                db = mysql.connector.connect(user=our_username, database=testname, password=our_password, host=our_host)
+                db = mysql.connector.connect(user=our_username, database=testname, password=our_password, host=our_host, port=our_port)
                 if db.is_connected():
                     our_dbname = testname
                     dbglog('Connected to MySQL database %s' % our_dbname)
@@ -566,7 +519,7 @@ if is_mysql and not forcedbname:
         exit_on_error()
 elif is_mysql and forcedbname:
     try:
-        db = mysql.connector.connect(user=our_username, database=forcedname, password=our_password, host=our_host)
+        db = mysql.connector.connect(user=our_username, database=forcedname, password=our_password, host=our_host, port=our_port)
         if db.is_connected():
             our_dbname = forcedname
             dbglog('Connected to forced MySQL database %s' % forcedname)
@@ -658,9 +611,9 @@ if not no_sources:
     if bookmarks:
         my_command = my_command + ' AND idFile NOT IN (SELECT idFile FROM bookmark)'
         our_source_list = our_source_list + 'Keeping bookmarked files '
-        
+
         # construct the full SQL query
-        
+
     sql = \
         """DELETE FROM files WHERE idPath IN(SELECT idPath FROM path where (""" + my_command + """));"""
 if no_sources:
@@ -673,7 +626,7 @@ if no_sources:
     if bookmarks:
         if my_command:
             my_command = my_command + ' AND idFile NOT IN (SELECT idFile FROM bookmark)'
-        else: 
+        else:
             my_command = my_command + ' idFile NOT IN (SELECT idFile FROM bookmark)'
         our_source_list = our_source_list + 'Keeping bookmarked files '
     if excluding:
@@ -682,7 +635,7 @@ if no_sources:
         else:
             my_command = my_command + exclude_command.replace('AND','',1)
         our_source_list = our_source_list + 'Keeping items from excludes.xml '
-            
+
 # Build SQL query
 
 if not no_sources: # this is SQL for no sources
@@ -693,7 +646,7 @@ else:
 #   sql2= """DELETE FROM path WHERE idPath IN (SELECT * FROM( SELECT idPath FROM path WHERE (strPath LIKE 'rtmp://%' OR strPath Like 'rtmpe:%' OR strPath LIKE 'plugin:%' OR strPath LIKE 'http://%') as pathsub);"""   
 dbglog('SQL command is %s' % sql)
 if not specificpath and not replacepath:
-    dbglog (our_source_list)            
+    dbglog (our_source_list)
     our_select = sql.replace('DELETE FROM files','SELECT strPath FROM path',1)
     if bookmarks:  # have to delete from paths table rather than files as there is a conflicting trigger on the files table
         our_select = sql.replace('DELETE FROM files', 'SELECT strPath FROM path WHERE idPath in (SELECT idPath FROM files', 1)
@@ -720,7 +673,7 @@ xbmc.sleep(500)
 
 if promptdelete:
     cleaner_log_file(our_select, False)
-    mydisplay = MyClass('script.database.cleaner-summary.xml', addonpath)
+    mydisplay = MyClass('cleaner-window.xml', addonpath, 'Default', '1080i')
     mydisplay.doModal()
     del mydisplay
     if flag == 1:
@@ -766,7 +719,7 @@ if i:
         # Commit the changes in the database
 
             db.commit()
-            
+
         except Exception as e:
 
         # Rollback in case there is any error
@@ -777,7 +730,7 @@ if i:
             dbglog('**  SQL ERROR  **  SQL ERROR   **  SQL ERROR  **  SQL ERROR  **  SQL ERROR  **')
             dbglog('**   %s ' % e)
             dbglog('******************************************************************************')
-            
+
     else:
         dbglog('Changing Paths - generating SQL statements')
         our_select = "SELECT strPath from path WHERE strPath Like '" + old_path + "%'"
@@ -790,11 +743,10 @@ if i:
         dialog.update(1)
         dbglog('Cursor size is %d' % listsize)
         cursor.execute(our_select)
-        renamepath_list = [] 
+        renamepath_list = []
         for strPath in cursor:  # build a list of paths to change
             renamepath_list.append( ''.join(strPath))
-            
-            
+
         for i in range(len(renamepath_list)):
             tempcount += 1
             our_old_path = renamepath_list[i]
@@ -819,7 +771,7 @@ if i:
     db.close()
     dbglog("Database connection closed")
     # Make sure replacing or changing a path is a one-shot deal
-    
+
     if replacepath or specificpath:
         addon.setSetting('specificpath', 'false')
         addon.setSetting('replacepath', 'false')
@@ -843,7 +795,7 @@ if i:
                             'Script finished.  You should run clean library for best results'
                             )
     dbglog('Script finished')
-    
+
 else:
     xbmcgui.Dialog().notification(addonname, 'Script aborted - No changes made', xbmcgui.NOTIFICATION_INFO, 3000)
     dbglog('script aborted by user - no changes made')
@@ -851,6 +803,3 @@ else:
     exit(1)
 WINDOW.setProperty('database-cleaner-running', 'false')
 
-        
-
-   
